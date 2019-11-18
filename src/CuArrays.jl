45d5a70cfd743f222c9d24842594251834425a32
Tim Besard <tim.besard@gmail.com>
2019-11-18 19:07:25 +0100

    Lower  version check.

--- src/CuArrays.jl
+++ src/CuArrays.jl
@@ -103,8 +103,8 @@ function __init__()
         # library compatibility
         if has_cutensor()
             ver = CUTENSOR.version()
-            if ver.major != 1 || ver.minor != 0
-                error("CuArrays.jl only supports CUTENSOR 1.0")
+            if !(ver.major == 0 && ver.minor == 3)
+                error("CuArrays.jl only supports CUTENSOR 0.3")
             end
         end
 
